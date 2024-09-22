e8bc736518871eaf12bf6daac18aec032ae09fde
mulearnlbs <mulearnlbs@gmail.com>
2024-09-22 16:45:05 +0530

    Update Events.tsx

--- src/Components/Events/Events.tsx
+++ src/Components/Events/Events.tsx
@@ -6,19 +6,8 @@ const Events = () => {
     <div className={styles.events}>
       <h2>Our Event Journey</h2>
       <div className={styles.innerDiv}>
-        <div className={styles.card + " " + styles.large}>
-          <div className={styles.content}>
-            <div className={styles.date}>
-              <span>{data["events"][0].month}</span>
-              <span>{data["events"][0].date}</span>
-            </div>
-            <div className={styles.text}>
-              <strong>{data["events"][0].head}</strong>
-              <span>{data["events"][0].para}</span>
-            </div>
-          </div>
-          <img src={data["events"][0].img} alt="" />
-        </div>
+
+        
         <div className={styles.subContent}>
           {data.events.slice(1).map((event, index) => {
             return (
