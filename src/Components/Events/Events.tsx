ba22b26da2f1995b4475e65538b0f0d7293ee0a4
mulearnlbs <mulearnlbs@gmail.com>
2024-09-22 16:36:39 +0530

    Update Events.tsx

--- src/Components/Events/Events.tsx
+++ src/Components/Events/Events.tsx
@@ -19,7 +19,25 @@ const Events = () => {
           </div>
           <img src={data["events"][0].img} alt="" />
         </div>
-     
+       <div className={styles.subContent}>
+          {data.events.slice(1).map((event, index) => {
+            return (
+              <div className={styles.card + " " + styles.small} key={index}>
+                <div className={styles.content}>
+                  <div className={styles.date}>
+                    <span>{event.month}</span>
+                    <span>{event.date}</span>
+                  </div>
+                  <div className={styles.text}>
+                    <strong>{event.head}</strong>
+                    <span>{event.para}</span>
+                  </div>
+                </div>
+                <img src={event.img} alt="" />
+              </div>
+            );
+          })}
+        </div>
        
       </div>
     </div>
