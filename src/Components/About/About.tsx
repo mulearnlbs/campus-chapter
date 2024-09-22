500261e1a47fafb23656652ac480f8e7a716e2c3
mulearnlbs <mulearnlbs@gmail.com>
2024-09-22 20:46:13 +0530

    Update About.tsx

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -4,7 +4,7 @@ import data from "../../../data.json"
 const About = () => {
     return (
         <div id="about" className={styles.AboutWrapper}>
-            
+            <h1>About us</h1>
             <p>{data.about}</p>
         </div>
     );
